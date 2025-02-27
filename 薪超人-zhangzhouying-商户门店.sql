41967e2d625d5c91b5c869f9899cb11e7a9625d1
githubsansan <c7-h@outlook.com>
2025-02-27 15:28:46 +0800

    修改：修改C端用户和B端用户查询

--- 薪超人-zhangzhouying-商户门店.sql
+++ 薪超人-zhangzhouying-商户门店.sql
@@ -1,9 +1,37 @@
---head:"tenant_id	company_name	store_id	store_name	address"
+--head:"store_id	store_name	contact_person_name	address"
 select
-    tenant_id
-  , company_name
-  , id as store_id
+    id as store_id
   , store_name
+  , contact_person_name
   , address
-from tenant_super
+from tenant_super_stores
 where deleted_flag = 0 
+and id in (    
+  20909
+, 20355
+, 20355
+, 20355
+, 19217
+, 19217
+, 18043
+, 17861
+, 20935
+, 20935
+, 20945
+, 20945
+, 20945
+, 17861
+
+, 17861
+, 20935
+, 20935
+, 19217
+, 19217
+, 20931
+, 18599
+, 18599
+, 17995
+, 17995
+, 17999
+, 18153
+, 18575)
